ab9b5c73516e5c09c4e95f8a9c6e6df6b7b60b76
Avika Lohany <60224735+avikalohany99@users.noreply.github.com>
2021-06-17 09:34:55 -0400

    Switched html part to calculation.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
 <template>
   <div id="app">
-    <div class="col-md-6">
-      <div class="calc-card center">
-        <h4 id="title">
-          {{title}}
-        </h4>
-        <div id="screen">"Type here"
-        <calculation />
-        </div>
-      </div>
-    </div>
+    <calculation />
   </div>
 </template>
 
@@ -18,11 +9,6 @@ import calculation from './components/Calculation.vue'
 
 export default {
   name: 'app',
-  data() {
-    return {
-      title: "CALCULATOR",
-    }
-  },
   components: {
     calculation
   }
