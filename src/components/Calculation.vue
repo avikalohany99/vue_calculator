a60029fd95586441a67c32e107aac60d8a7e3325
Avika Lohany <60224735+avikalohany99@users.noreply.github.com>
2021-06-20 11:29:06 -0400

    Added operator functionality

--- src/components/Calculation.vue
+++ src/components/Calculation.vue
@@ -1,60 +1,95 @@
 <template>
-<div>
+  <div>
     <div class="col-md-6">
       <div class="calc-card center">
         <h4 id="title">CALCULATOR</h4>
-        <div id="screen">{{ current_calc || 0}}</div>
+        <div id="screen">{{ current_calc || 0 }}</div>
         <div class="calculator__keys">
-          <button @click="num_display('7')">7</button>
-          <button @click="num_display('8')">8</button>
-          <button @click="num_display('9')">9</button>
-          <button class="operator">/</button>
+          <button @click="append('7')">7</button>
+          <button @click="append('8')">8</button>
+          <button @click="append('9')">9</button>
+          <button class="operator" @click="div_num">/</button>
           <div class="break"></div>
         </div>
         <div class="calculator__keys">
-          <button @click="num_display('4')">4</button>
-          <button @click="num_display('5')">5</button>
-          <button @click="num_display('6')">6</button>
-          <button class="operator">x</button>
+          <button @click="append('4')">4</button>
+          <button @click="append('5')">5</button>
+          <button @click="append('6')">6</button>
+          <button class="operator" @click="mul_num">x</button>
           <div class="break"></div>
         </div>
         <div class="calculator__keys">
-          <button @click="num_display('1')">1</button>
-          <button @click="num_display('2')">2</button>
-          <button @click="num_display('3')">3</button>
-          <button class="operator">-</button>
+          <button @click="append('1')">1</button>
+          <button @click="append('2')">2</button>
+          <button @click="append('3')">3</button>
+          <button class="operator" @click="sub_num">-</button>
           <div class="break"></div>
         </div>
         <div class="calculator__keys">
-          <button @click="num_display('0')">0</button>
+          <button @click="append('0')">0</button>
           <button class="btn_clr" @click="clr_display()">AC</button>
-          <button class="operator">=</button>
+          <button class="operator" @click="eql">=</button>
           <button class="operator" @click="add_num">+</button>
           <div class="break"></div>
-          
         </div>
       </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            current_calc: '',
-        }
+  data() {
+    return {
+      current_calc: "",
+      result1: null,
+      result2: null,
+      operator: null,
+      operatorClicked: false,
+    };
+  },
+  methods: {
+    clr_display() {
+      this.current_calc = "";
     },
-    methods: {
-        num_display(num) {
-                this.current_calc = `${this.current_calc}${num}`;
-        },
-        clr_display() {
-                this.current_calc = '';
-        },
-        add_num() {
-                this.current_calc.append('+');
-        }
-    }
-}
+    append(num) {
+      if (this.operatorClicked) {
+        this.current_calc = "";
+        this.operatorClicked = false;
+      }
+      this.current_calc = `${this.current_calc}${num}`;
+    },
+    add_num() {
+      this.operator = (a, b) => a + b;
+      this.append("+");
+      this.result1 = this.current_calc;
+      this.operatorClicked = true;
+    },
+    sub_num() {
+      this.operator = (a, b) => a - b;
+      this.append("-");
+      this.result1 = this.current_calc;
+      this.operatorClicked = true;
+    },
+    mul_num() {
+      this.operator = (a, b) => a * b;
+      this.append("*");
+      this.result1 = this.current_calc;
+      this.operatorClicked = true;
+    },
+    div_num() {
+      this.operator = (a, b) => a / b;
+      this.append("/");
+      this.result1 = this.current_calc;
+      this.operatorClicked = true;
+    },
+    eql() {
+      this.current_calc = `${this.operator(
+        parseFloat(this.current_calc),
+        parseFloat(this.result1)
+      )}`;
+      this.result1 = null;
+    },
+  },
+};
 </script>
